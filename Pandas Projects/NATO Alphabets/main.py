33f88538bee8d1e9dfd3b1695038af40394c8c99
Muhammad Taleeb Raza <Muhammadtaleebraza1@gmail.com>
2025-08-27 16:44:29 +0500

    Working Password Generator App -- GUI made in CustomTkinter -- Functionality of saving password into a file. -- Able to search saved passwords -- Fully offline to ensure security

--- Pandas Projects/NATO Alphabets/main.py
+++ Pandas Projects/NATO Alphabets/main.py
@@ -4,7 +4,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_phonetic_dict = {row_data.letter.lower():row_data.code.lower() for index, row_data in data.iterrows()}
 
-user_word = input("Enter a word: ").lower()
-letter_coded = [nato_phonetic_dict[letter] for letter in user_word]
+def generate_phonetics():
+    while True:
+        user_word = input("Enter a word: ").lower()
 
-print(letter_coded)
+        try:
+            letter_coded = [nato_phonetic_dict[letter] for letter in user_word]
+        except KeyError:
+            print("Enter Only Alphabets")
+        else:
+            print(letter_coded)
+            break
+
+generate_phonetics()
--- Tkinter projcts/Password Manager/main.py
+++ Tkinter projcts/Password Manager/main.py
@@ -1,4 +1,6 @@
 # Importing Packages
+import json
+
 from pyperclip import copy
 from tkinter import PhotoImage
 from customtkinter import *
@@ -32,45 +34,89 @@ def show_password():
         password_entry.insert(index=END, string=password[letter])
     hide_button.configure(command=hide_password, text="Hide")
 
+def focus_next(event):
+    current_widget = event.widget.master
+
+    if current_widget == website_entry:
+        email_username_entry.focus_set()
+    elif current_widget == email_username_entry:
+        password_entry.focus_set()
+    elif current_widget == password_entry:
+        generate_button.focus_set()
+    return "break"
+
+# ------------------ Search Function ------------------ #
+def search_data():
+    try:
+        with open("Passwords.json", mode="r") as file:
+            password_data = json.load(file)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        messagebox.showinfo(title="File Not Found", message="We Couldn't Find Any File With Your Passwords\nThis can happen due to\n1. File Name Being Changed\n2. File Being Deleted\n3. File Being Empty.")
+    else:
+        website_to_search = website_entry.get().lower()
+        if website_to_search in password_data:
+            messagebox.showinfo(title="Data Found", message=f"Email: {password_data[website_to_search]["email"]}\nPassword: {password_data[website_to_search]["password"]}\n\n(password copied to clipboard)")
+            copy(password_data[website_to_search]["password"])
+        if website_to_search not in password_data:
+            messagebox.showinfo(title="Data Not Found", message="There was no data linked with that website")
+
+
 # ------------------ Password Generator ------------------ #
 def generate_password():
     global password
     password_entry.delete(first_index=0, last_index=END)
     password = create_password()
-    password_string = ''.join(password)
-    password_entry.insert(index=0, string=password_string)
-    copy(password_string) # Copy password on the clipboard
+    password = ''.join(password)
+    password_entry.insert(index=0, string=password)
+    copy(password) # Copy password on the clipboard
     hide_button.configure(command=hide_password, text="Hide")
 
 # ------------------ Save Password ------------------ #
 def write_to_file(website:str, name_email:str, password:str):
-    with open("Passwords.txt", mode="a") as file:
-        text_to_write = website + " | " + name_email + " | " + password + "\n"
-        file.write(text_to_write)
+    data_to_write = {website.lower(): {
+        "email": name_email,
+        "password": password
+    }}
+
+    try:
+        with open("Passwords.json", mode="r") as file:
+            new_data = json.load(file)
+            new_data.update(data_to_write)
+
+        with open("Passwords.json", mode="w") as file:
+            json.dump(new_data, file, indent=4)
+
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        with open("Passwords.json", mode="w") as file:
+            json.dump(data_to_write, file, indent=4)
+
 
 def save_password():
+    global password
     website = website_entry.get()
     email_username = email_username_entry.get()
-    password = password_entry.get()
 
-    if len(website) < 2 or len(email_username) < 5 or len(password) < 5: # don't let user save if nothing is typed
+    if len(website) == 0 or len(email_username) == 0 or len(password) == 0: # don't let user save if nothing is typed
         filed_to_fill = ""
-        if len(website) < 2:
+        if len(website) == 0:
             filed_to_fill = "Website"
-        elif len(email_username) < 5:
-            filed_to_fill = "Email/Username"
+        elif len(email_username) == 0:
+            filed_to_fill = "Email/Username "
+        elif len(password) == 0:
+            filed_to_fill = "Password "
 
         messagebox.showinfo(title="No Data Found", message=f"Please Enter {filed_to_fill} To Save")
 
     else:
         # ask the user to confirm data saving
-        add_data = messagebox.askokcancel(title=website, message=f"Email: {email_username}\nPassword: {password}\nPress OK to add.")
+        add_data = messagebox.askokcancel(title=website, message=f"Email/Username: {email_username}\nPassword: {password}\nPress OK to add.")
 
         if add_data: # if user confirms
             write_to_file(website=website, name_email=email_username, password=password)
             website_entry.delete(0, END)
             email_username_entry.delete(0, END)
             password_entry.delete(0, END)
+            password = ''
         else:
             messagebox.showinfo(title="Operation Cancelled", message="No Data Was Added In The Password File")
 
@@ -106,23 +152,31 @@ email_username_label.grid(column=0, row=2)
 password_label.grid(column=0, row=3)
 
 # Creating required entries
-website_entry = CTkEntry(master=app, width=450, text_color=YELLOW, font=(FONT, 15, "bold"), bg_color=RICH_BLACK, fg_color=TRANSPARENT, border_color=VIOLET, placeholder_text= "Enter Any Website", placeholder_text_color=VIOLET)
+website_entry = CTkEntry(master=app, width=295, text_color=YELLOW, font=(FONT, 15, "bold"), bg_color=RICH_BLACK, fg_color=TRANSPARENT, border_color=VIOLET, placeholder_text= "Enter Any Website", placeholder_text_color=VIOLET)
 email_username_entry = CTkEntry(master=app, width=450, text_color=YELLOW, font=(FONT, 15, "bold"), bg_color=RICH_BLACK, fg_color=TRANSPARENT, border_color=VIOLET, placeholder_text= "Enter Your Email/Username", placeholder_text_color=VIOLET)
 password_entry = CTkEntry(master=app, width=300, text_color=YELLOW, font=(FONT, 15, "bold"), bg_color=RICH_BLACK, fg_color=TRANSPARENT, border_color=VIOLET, placeholder_text="Press Generate", placeholder_text_color=VIOLET)
 
 # Placing the entries
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1)
 email_username_entry.grid(column=1, row=2, columnspan=2)
 password_entry.grid(column=1, row=3)
+
+# Bind Enter key to move to the next field
+website_entry.bind(sequence="<Return>", command=focus_next)
+email_username_entry.bind(sequence="<Return>", command=focus_next)
+password_entry.bind(sequence="<Return>", command=focus_next)
 
 # Creating required buttons
 generate_button = CTkButton(master=app, bg_color=RICH_BLACK, fg_color=TRANSPARENT, border_color=LIGHT_CORAL, hover_color=YELLOW, text_color=LIGHT_CORAL, text="Generate Password", border_width=1, font=(FONT, 15, "bold"), command=generate_password)
 add_button = CTkButton(master=app, width=300, bg_color=RICH_BLACK, fg_color=TRANSPARENT, border_color=VIOLET, hover_color=LIGHT_CORAL, text_color=YELLOW, text="Add", border_width=1, font=(FONT, 15, "bold"), command=save_password)
-hide_button = CTkButton(master=app, width=155, bg_color=RICH_BLACK, fg_color=TRANSPARENT, border_color=LIGHT_CORAL, hover_color=LIGHT_CORAL, text_color=YELLOW, text="Hide", border_width=1, font=(FONT, 15, "bold"), command=hide_password)
+hide_button = CTkButton(master=app, width=150, bg_color=RICH_BLACK, fg_color=TRANSPARENT, border_color=LIGHT_CORAL, hover_color=LIGHT_CORAL, text_color=YELLOW, text="Hide", border_width=1, font=(FONT, 15, "bold"), command=hide_password)
+search_button = CTkButton(master=app, width=150, bg_color=RICH_BLACK, fg_color=TRANSPARENT, border_color=LIGHT_CORAL, hover_color=LIGHT_CORAL, text_color=YELLOW, text="Search", border_width=1, font=(FONT, 15, "bold"), command=search_data)
+
 # Placing the buttons
 add_button.grid(column=1, row=4)
 generate_button.grid(column=2, row=3)
 hide_button.grid(column=2, row=4)
+search_button.grid(column=2, row=1)
 
 # Main app loop
 app.mainloop()
